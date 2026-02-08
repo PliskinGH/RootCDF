a75a540c11535beb0d117ecc0cedf4797211e3f6
Pliskin <nabil14@gmail.com>
2026-02-08 16:37:15 +0100

    import of html

--- RootCDF/settings.py
+++ RootCDF/settings.py
@@ -44,7 +44,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django.contrib.sites',
+    'django.contrib.flatpages',
+    'django.contrib.sitemaps',
+    'fullurl',
     'tinymce',
+    'cdf',
 ]
 
 MIDDLEWARE = [
---
+++ cdf/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CdfConfig(AppConfig):
+    name = 'cdf'
